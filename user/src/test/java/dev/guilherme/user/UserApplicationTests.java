70d0c9ea20a5f77e156cb7ac86d7971eb0ff2ef1
GuilhermeD9 <sgsouza.173@gmail.com>
2025-07-25 09:38:01 -0300

    feat: Initial Commit

---
+++ user/src/test/java/dev/guilherme/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilherme.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
