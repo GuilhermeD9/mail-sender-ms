4158efe42e805e27837d40081531082a15e9859c
GuilhermeD9 <sgsouza.173@gmail.com>
2025-08-07 10:37:24 -0300

    feat: Email sender configuration

--- email/src/main/java/dev/guilherme/email/consumer/EmailConsumer.java
+++ email/src/main/java/dev/guilherme/email/consumer/EmailConsumer.java
@@ -1,7 +1,9 @@
 package dev.guilherme.email.consumer;
 
 import dev.guilherme.email.dto.EmailDto;
+import dev.guilherme.email.entity.EmailEntity;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,7 @@ public class EmailConsumer {
 
     @RabbitListener(queues = "email-queue")
     public void listenEmailQueue(@Payload EmailDto emailDto) {
-        System.out.println(emailDto.emailTo());
+        var emailEntity = new EmailEntity();
+        BeanUtils.copyProperties(emailDto, emailEntity);
     }
 }
